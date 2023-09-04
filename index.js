17e11563595b6990da2a8bec7020cb1ddeb8fc1c
leandrotop <62478436+leandrotop@users.noreply.github.com>
2023-09-03 21:01:48 -0300

    update teste

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var servidor = {}
 servidor.user = 'abc'
 servidor.senha = 'abc'  
 
-app.use(express.static('public/'));
+app.use(express.static('public/teste/'));
 
 var servidor = {}
 io.on('connection', (socket) => {
